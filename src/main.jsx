86f71312eba16a839ff5de6e74f641c784d2622f
Bryan Berland <b.berland01@gmail.com>
2023-08-08 21:30:45 -0400

    added gh actions

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,18 @@ import App from './App.jsx'
 import './index.css'
 import {
   createHashRouter,
+  createBrowserRouter,
   RouterProvider
 } from 'react-router-dom'
 
-const router = createHashRouter([
+const routes = [
   {
     path: "/*",
     element: <App />,
   }
-])
+]
+
+const router = createBrowserRouter(routes, { basename: import.meta.env.DEV ? '/' : '/bbr-store/' })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
