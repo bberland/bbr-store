bea5b64c617eaa79c021db4134f20b8965252b3e
Bryan Berland <b.berland01@gmail.com>
2023-08-11 21:33:22 -0400

    added netlify cli

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {
-  createHashRouter,
   createBrowserRouter,
   RouterProvider
 } from 'react-router-dom'
